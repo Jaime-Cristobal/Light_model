dcebe11d6b6345292c21f8bfd597eab809d7c33a
Jaime <cristobj@coyote.csusb.edu>
2020-08-09 22:14:35 -0700

    Implemented mesh processing with helper func

--- Model.cpp
+++ Model.cpp
@@ -15,8 +15,14 @@ using std::vector;
 /**
 *
 */
-Model::Model(std::string const& path, bool gamma) : gammaCorrection(gamma)
+Model::Model(string const& path, int texCoordAmnt, bool gamma) : gammaCorrection(gamma), texCoordAmnt(texCoordAmnt)
 {
+	if (this->texCoordAmnt <= 0)
+	{
+		cout << "WARNING::MODEL_H::LMT::MODEL. Variable texCoordAmnt cannot be less than 1! Value is now defaulted to 1" << endl;
+		texCoordAmnt = 1;
+	}
+
 	loadModel(path);
 }
 
@@ -24,11 +30,15 @@ Model::Model(std::string const& path, bool gamma) : gammaCorrection(gamma)
 /**
 *
 */
-void Model::loadModel(std::string path)
+void Model::loadModel(string path)
 {
 	// assimp has ownership of the pointer so it will delete the resource once
-	// it's destructor is invoked once this function loadModel(...) is out of scope.
-	// Using assimp postprocessing for speed optimizations.
+	// it's destructor is invoked. It's invoked once this function loadModel(...) is out of scope.
+	//
+	// NOTE: Using a smart pointer here will result in a segmentation fault which is fun if you're 
+	//		 into that sort of thing.
+	//
+	// Using assimp postprocessing for speed optimizations on getting those faces.
 	Assimp::Importer import;
 	aiScene const* scene = import.ReadFile(path, aiProcess_Triangulate | aiProcess_FlipUVs | aiProcess_CalcTangentSpace);
 
@@ -44,7 +54,69 @@ void Model::loadModel(std::string path)
 	processNode(scene->mRootNode, scene);
 }
 
+
+/**
+*
+*/
 Mesh Model::processMesh(aiMesh* mesh, aiScene const* scene)
 {
-	
+	vector<Vertex> vertices;
+	vector<unsigned int> indices;
+	vector<Texture> textures;
+
+	// get vertex positions, normals, tangets, bitangents, and texture coordinates (if present)
+	for (unsigned int i = 0; i < mesh->mNumVertices; i++)
+	{
+		Vertex vertex;
+		
+		vertex.position = get3DVectorAttr(mesh->mVertices[i]);
+		vertex.normal = get3DVectorAttr(mesh->mNormals[i]);
+		vertex.tangent = get3DVectorAttr(mesh->mTangents[i]);
+		vertex.bitangent = get3DVectorAttr(mesh->mBitangents[i]);
+
+		// Unlike vertex position and normal, we have to test if texture coordinates are present
+		// in the mesh as it does not always contain one.
+		for (int texSet = 0; texSet < texCoordAmnt; texSet++)
+		{
+			if (mesh->mTextureCoords[texSet])
+				vertex.texCoord = get2DVectorAttr(mesh->mTextureCoords[texSet][i]);
+			else
+				vertex.texCoord = glm::vec2(0.0f, 0.0f);
+		}
+
+		vertices.push_back(vertex);
+	}
+
+	// Get the indices which are the faces
+	for (aiFace const& face : mesh->mFaces)		// NOTE: mFaces is a pointer, NOT AN ARRAY
+	{											// Note to self: try old school for loops using mNumFaces
+												// next time.
+	}
+}
+
+
+/**
+*
+*/
+glm::vec3 lmt::get3DVectorAttr(aiVector3D const& vec)
+{
+	glm::vec3 vector;
+	vector.x = vec.x;
+	vector.y = vec.y;
+	vector.z = vec.z;
+
+	return vector;
+}
+
+
+/**
+* Yes this says get 2D vector attributes but takes a 3D vector. 
+*/
+glm::vec2 lmt::get2DVectorAttr(aiVector3D const& vec)
+{
+	glm::vec2 vector;
+	vector.x = vec.x;
+	vector.y = vec.y;
+
+	return vector;
 }
--- Model.h
+++ Model.h
@@ -7,6 +7,7 @@
 
 #include <vector>
 #include <string>
+#include <algorithm>
 
 #include "Mesh.h"
 #include "stb_image.h"
@@ -18,6 +19,7 @@ namespace lmt
 	class Model
 	{
 	private:
+		int texCoordAmnt;
 		std::vector<Texture> textures_loaded;
 		std::vector<Mesh> meshes;
 		std::string directory;
@@ -29,7 +31,10 @@ namespace lmt
 		std::vector<Texture> loadMaterialTextures(aiMaterial* mat, aiTextureType type, std::string typeName);
 
 	public:
-		Model(std::string const& path, bool gamma = false);
+		Model(std::string const& path, int texCoordAmnt, bool gamma = false);
 		void draw(Shader& shader);
 	};
+
+	glm::vec3 get3DVectorAttr(aiVector3D const& vec);
+	glm::vec2 get2DVectorAttr(aiVector3D const& vec);
 }
